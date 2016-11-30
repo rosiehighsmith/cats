3eab4964bc0ffb67d665321ef2bfcf04b538a73a
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-30 16:56:33 +0000

    Alter cats table and add age and color columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130164402) do
+ActiveRecord::Schema.define(version: 20161130165457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161130164402) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "color"
+    t.integer  "age"
   end
 
 end
